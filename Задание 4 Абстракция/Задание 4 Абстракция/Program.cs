3c16ecff2c6a8a173524303499fd89386e1fbb66
Alexander Medvedev <alon.grgr@icloud.com>
2024-11-08 20:13:56 +0300

    Решил номер 4

---
+++ Задание 4 Абстракция/Задание 4 Абстракция/Program.cs
@@ -0,0 +1,27 @@
+﻿var rectangle = new Rectangle {Width = 5, Height = 4};
+var circle = new Circle { Radious = 10 };
+circle.GetArea();
+rectangle.GetArea();
+
+abstract class Shape 
+{
+    public abstract void GetArea();
+}
+class  Circle : Shape
+{
+    public double Radious { get; set; }
+    public override void GetArea()
+    {
+        Console.WriteLine($"Площадь круга: {Radious * Radious * 3.14}");
+    }
+}
+
+class Rectangle : Shape
+{
+    public double Width { get; set; }
+    public double Height { get; set; }
+    public override void GetArea()
+    { 
+        Console.WriteLine($"Площадь прямоугольника: {Width * Height}"); 
+    }
+}
