0c7eb120bd28150ee03ca35a5ed14fbe385cacaa
Alexander Medvedev <alon.grgr@icloud.com>
2024-11-09 23:02:12 +0300

    Решил номер Generics#3

---
+++ Generics 3/Generics 3/Emloyee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics_3
+{
+    public class Emloyee
+    {
+        public string Name { get; set; }
+        public Emloyee(string name)
+        {
+            Name = name;
+        }
+    }
+}
