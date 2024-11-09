7f461091c92f33f58cf6159314b2763a4d91707c
Alexander Medvedev <alon.grgr@icloud.com>
2024-11-09 23:17:30 +0300

    Решил Generics#4

---
+++ Generics 4/Generics 4/Program.cs
@@ -0,0 +1,16 @@
+﻿int a = 3, b = 4;
+Console.WriteLine($"До обмена: a = {a}; b = {b}");
+Swap<int>(ref a, ref b);
+Console.WriteLine($"После обмена: a = {a}; b = {b}");
+
+double c = 3.3, d = 4.6;
+Console.WriteLine($"До обмена: c = {c}; d = {d}");
+Swap<double>(ref c, ref d);
+Console.WriteLine($"После обмена: c = {c}; d = {d}");
+
+static void Swap<T>(ref T x, ref T y)
+{
+    T temp = x;
+    x = y;
+    y = temp;
+}
