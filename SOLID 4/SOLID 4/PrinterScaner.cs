42109719657697389c801fa8adef3a9d6036730e
Alexander Medvedev <alon.grgr@icloud.com>
2024-11-09 16:29:19 +0300

    Решил SOLID#4

---
+++ SOLID 4/SOLID 4/PrinterScaner.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID_4
+{
+    internal class PrinterScaner : IPrinterScaner
+    {
+        public void Print(string document)
+        {
+            Console.WriteLine($"Печать документа: {document}");
+        }
+        public void Scan(string document)
+        {
+            Console.WriteLine($"Сканировать документ: {document}");
+        }
+    }
+}
