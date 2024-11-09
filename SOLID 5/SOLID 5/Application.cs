96f5cee7ca5430713b99b70d185eee3119936580
Alexander Medvedev <alon.grgr@icloud.com>
2024-11-09 20:35:10 +0300

    Решил SOLID#5

---
+++ SOLID 5/SOLID 5/Application.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID_5
+{
+    public class Application
+    {
+        public ILogger logger { get; set; }
+        public Application(ILogger logger)
+        {
+            this.logger = logger;
+        }
+        public void Action()
+        {
+            logger.Log("Процесс начался");
+        }
+    }
+}
