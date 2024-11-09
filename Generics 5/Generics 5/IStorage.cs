1ad1983cd5d94c776f21ee86cacf99dcc547e21e
Alexander Medvedev <alon.grgr@icloud.com>
2024-11-09 23:51:20 +0300

    Решил Generics#5

---
+++ Generics 5/Generics 5/IStorage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics_5
+{
+    internal interface IStorage<T>
+    {
+        void Add(T item);
+        T Get(int index);
+    }
+}
---
+++ Generics 5/Generics 5/ListStorage.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics_5
+{
+    public class ListStorage<T> : IStorage<T>
+    {
+        private List<T> items;
+        public ListStorage() 
+        {
+            items = new List<T>();
+        }
+        public void Add(T item)
+        {
+                items.Add(item);
+        }
+        public T Get(int index)
+        {
+                return items[index];
+        }
+    }
+}
